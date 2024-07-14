c9e0dfd90d08fa9409a4c9c3cd0d4e5ce8123a74
Nicolás Fuenzalida Contreras <na.fuenzalida.contreras@gmail.com>
2024-07-14 17:57:03 -0400

    reporte constacia de empleado

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrinterService } from '../printer/printer.service';
-import { getHelloWorldReport } from 'src/reports';
+import { PrinterService } from 'src/printer/printer.service';
+import {
+  getEmploymentLetterByIdReport,
+  getEmploymentLetterReport,
+  getHelloWorldReport,
+} from 'src/reports';
 
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
@@ -16,6 +20,39 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
 
   hello() {
     const docDefinition = getHelloWorldReport();
+    const doc = this.printerService.createPdf(docDefinition);
+
+    return doc;
+  }
+
+  employmentLetter() {
+    const docDefinition = getEmploymentLetterReport();
+    const doc = this.printerService.createPdf(docDefinition);
+    return doc;
+  }
+
+  async employmentLetterById(employeeId: number) {
+    const employee = await this.employees.findUnique({
+      where: {
+        id: employeeId,
+      },
+    });
+
+    if (!employee) {
+      throw new NotFoundException(`Employee with id ${employeeId} not found`);
+    }
+
+    const docDefinition = getEmploymentLetterByIdReport({
+      employerName: 'Nicolás Fuenzalida',
+      employerPosition: 'Gerente de RRHH',
+      employeeName: employee.name,
+      employeePosition: employee.position,
+      employeeStartDate: employee.start_date,
+      employeeHours: employee.hours_per_day,
+      employeeWorkSchedule: employee.work_schedule,
+      employerCompany: 'Tucan Code Corp.',
+    });
+
     const doc = this.printerService.createPdf(docDefinition);
 
     return doc;
--- src/printer/printer.service.ts
+++ src/printer/printer.service.ts
@@ -5,9 +5,9 @@ import type { BufferOptions, TDocumentDefinitions } from 'pdfmake/interfaces';
 const fonts = {
   Roboto: {
     normal: 'fonts/Roboto-Regular.ttf',
-    bold: '/fonts/Roboto-Medium.ttf',
-    italics: '/fonts/Roboto-Italic.ttf',
-    bolditalics: '/fonts/Roboto-MediumItalic.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf',
   },
 };
 
